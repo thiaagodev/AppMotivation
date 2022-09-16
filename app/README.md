ff390bc03daf0a924d8a9c41c3ea133db0e81382
Thiago Ferreira <thiaago.dev@gmail.com>
2022-09-16 17:47:41 -0300

    activity_main layout

---
+++ app/README.md
@@ -0,0 +1,5 @@
+# Aplicativo Motivation
+
+Aplicativo criado para aprofundar os fundamentos de Android com Kotlin.
+
+Curso: https://www.udemy.com/course/curso-desenvolvedor-kotlin
---
+++ app/src/main/java/com/thiaagodev/motivation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.thiaagodev.motivation
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
