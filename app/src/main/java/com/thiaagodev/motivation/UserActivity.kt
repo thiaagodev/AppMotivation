d5ddaf993a44c06262eee8278496b0c8e73cc4c6
Thiago Ferreira <thiaago.dev@gmail.com>
2022-09-24 20:01:54 -0300

    navegação entre activitys

--- app/src/main/java/com/thiaagodev/motivation/UserActivity.kt
+++ app/src/main/java/com/thiaagodev/motivation/UserActivity.kt
@@ -1,8 +1,10 @@
 package com.thiaagodev.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.thiaagodev.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
@@ -20,8 +22,20 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(view: View) {
-        if(view.id == R.id.button_save_name) {
-            TODO("")
+        if (view.id == R.id.button_save_name) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+
+
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
         }
     }
 }
