8a374a34e32b8b720bf30dee0eeb2b3f1d4014a2
Thiago Ferreira <thiaago.dev@gmail.com>
2022-09-24 18:47:57 -0300

    view binding

--- app/src/main/java/com/thiaagodev/motivation/MainActivity.kt
+++ app/src/main/java/com/thiaagodev/motivation/MainActivity.kt
@@ -3,11 +3,27 @@ package com.thiaagodev.motivation
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import com.thiaagodev.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNewWord.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_word) {
+            TODO("")
+        }
     }
 }
